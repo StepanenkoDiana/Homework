309e2d70e3648f1ece13ce22b8db3e265cdb3edf
StepanenkoDiana <137941630+StepanenkoDiana@users.noreply.github.com>
2023-07-10 18:39:56 +0200

    создание классов и калькулятор

---
+++ Homework_JAVA_1/src/Homework.java
@@ -0,0 +1,31 @@
+public class Homework {
+    public static void main(String...args){
+        String journal = "Hogwarts";
+        System.out.println(journal);
+
+        String  journal2 = "Harry Potter";
+        System.out.println(journal2);
+
+        String journal3 = journal + journal2;
+        System.out.println(journal3);
+
+        int numeral = 3;
+        int numeral2 = 6;
+        int numeral3 = numeral + numeral2;
+
+        System.out.println(numeral);
+        System.out.println(numeral2);
+        System.out.println(numeral3);
+
+        int sumOfTwoNumbers = numeral + numeral2;
+        int difOfTwoNumbers = numeral2 - numeral;
+        int quoOfTwoNumbers = numeral3 / numeral;
+        int proOfTwoNumbers = numeral2 * numeral3;
+
+        System.out.println(sumOfTwoNumbers);
+        System.out.println(difOfTwoNumbers);
+        System.out.println(quoOfTwoNumbers);
+        System.out.println(proOfTwoNumbers);
+
+    }
+}
